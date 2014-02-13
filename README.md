efd53a5bb3faba5fb15011722714efdc1855318b
Turntablelover <darthvader438@gmail.com>
2014-02-13 15:40:21 -0500

    Beginning Code

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Toontown-Central
 ================
 
 Remake of Toontown Central from Disney's Toontown Online with Panda3D 1.8.1
+
+Version: 1.0.0 Beta
---
+++ Toontown Central.py
@@ -0,0 +1,20 @@
+from direct.actor.Actor import Actor
+from pandac.PandaModules import *
+from direct.task import Task
+import math
+from math import pi, sin, cos
+from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
+from direct.interval.IntervalGlobal import Sequence
+from pandac.PandaModules import Point3
+from pandac.PandaModules import *
+ 
+import direct.directbase.DirectStart
+from panda3d.core import CollisionTraverser,CollisionNode
+from panda3d.core import CollisionHandlerQueue,CollisionRay
+from panda3d.core import Filename,AmbientLight,DirectionalLight
+from panda3d.core import PandaNode,NodePath,Camera,TextNode
+from panda3d.core import Vec3,Vec4,BitMask32
+from direct.gui.OnscreenText import OnscreenText
+from direct.actor.Actor import Actor
+from direct.showbase.DirectObject import DirectObject
